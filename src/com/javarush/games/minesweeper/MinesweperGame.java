e14afe3a4b4d7325b643aaa70f68ac65111392c2
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-01-27 15:01:06 +0100

    added 2 tasks

---
+++ src/com/javarush/games/minesweeper/MinesweperGame.java
@@ -0,0 +1,11 @@
+package com.javarush.games.minesweeper;
+import com.javarush.engine.cell.Game;
+
+public class MinesweperGame extends Game {
+
+    @Override
+    public void initialize() {
+
+    }
+
+}
